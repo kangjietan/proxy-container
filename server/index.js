e0f3f570aa3d1646422f2435b3d5c3a3e991938c
Kang Jie Tan <kangjietan@gmail.com>
2020-02-08 14:10:42 -0800

    3rd component

--- server/index.js
+++ server/index.js
@@ -18,25 +18,25 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(PORT, () => { console.log(`Listening on PORT: ${PORT}`); });
 
-app.get('/bottom', (req, res) => {
-  axios.get('http://13.56.50.57:3000/bundle.js')
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+// app.get('/bottom', (req, res) => {
+//   axios.get('http://13.56.50.57:3000/bundle.js')
+//     .then(response => {
+//       res.send(response.data);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// });
 
-app.get('/comments', (req, res) => {
-  axios.get('http://18.144.144.76:3001/bundle.js')
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+// app.get('/comments', (req, res) => {
+//   axios.get('http://18.144.144.76:3001/bundle.js')
+//     .then(response => {
+//       res.send(response.data);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// });
 
 app.get('/api/comments', (req, res) => {
   axios.get('http://18.144.144.76:3001/api/comments')
